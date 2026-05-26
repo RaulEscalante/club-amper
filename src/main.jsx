896b252760c67b4164e16b4dfd17e8682567c30d
Raul Escalante <raulescalantem14@gmail.com>
2026-05-26 09:13:35 -0500

    Use custom DB vars

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
+import ScrollToTop from './components/ScrollToTop.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <BrowserRouter>
+      <ScrollToTop/>
         <App />
       </BrowserRouter>
     </AuthProvider>
