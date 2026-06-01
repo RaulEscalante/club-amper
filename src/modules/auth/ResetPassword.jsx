1172f2ceeff3c6cdead363624e259ed2fc2a1383
Raul Escalante <raulescalantem14@gmail.com>
2026-06-01 09:25:14 -0500

    Forgot password

---
+++ src/modules/auth/ResetPassword.jsx
@@ -0,0 +1,9 @@
+function ResetPassword() {
+    return (
+        <div>
+            <h1>Reset Password</h1>
+            {/* Aquí iría el formulario para ingresar la nueva contraseña */}
+        </div>
+    );
+}
+export default ResetPassword
